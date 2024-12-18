5cc7e67976eea14b1753b530a10e2e50f9e782f5
unexpected-qa <adrian.pothuaud@happn.fr>
2024-12-19 00:08:34 +0100

    Set dynamic baseUrl for GitHub Actions in Cypress config
    Updated Cypress configuration to use a different baseUrl when running in the GitHub Actions environment. This ensures compatibility with differing local server ports in CI versus local development.

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 import {defineConfig} from "cypress";
 
+const isGithubActions = !!process.env.GITHUB_ACTIONS;
+
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: isGithubActions ? "http://localhost:4173" : "http://localhost:5173",
     projectId: "c8e8t2",
     setupNodeEvents(on, config) {
       // implement node event listeners here
